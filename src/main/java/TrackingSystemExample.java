7d029ff08720bc840ecf4ba82df48610c9692a0a
William <somemail>
2017-12-01 13:09:07 -0600

    Added an example use case
    src/main/java/TrackingSystemExample.java

---
+++ src/main/java/TrackingSystemExample.java
@@ -0,0 +1,16 @@
+import trackingsystem.TrackingSystem;
+import trackingsystem.entities.Passenger;
+import trackingsystem.entities.PassengerType;
+
+public class TrackingSystemExample {
+	public static void main(String[] args) {
+		TrackingSystem system = new TrackingSystem();
+		system.addPassenger(new Passenger(PassengerType.Commuter, 0, 3, false, true));
+		system.addPassenger(new Passenger(PassengerType.Commuter, 0, 5, false, true));
+		system.addPassenger(new Passenger(PassengerType.Commuter, 0, 4, true, false));
+		system.addPassenger(new Passenger(PassengerType.Vacationer, 90, 0, false));
+		system.addPassenger(new Passenger(PassengerType.Vacationer, 199, 0, true));
+		System.out.printf("Newspapers: %d%nMeals: %d%nTotal Fare: %,.2f%n", system.getPaperCount(),
+				system.getMealCount(), system.getTotalFare());
+	}
+}
